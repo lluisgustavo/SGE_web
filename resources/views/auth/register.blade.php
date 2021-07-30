c08bdfe18cd906d8ad765c202d63a94a45cb37dc
Luis Sousa <luis.sousa@giftconsult.com.br>
2021-07-30 11:08:56 -0300

    Added jQuery CEP fetch

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -139,7 +139,7 @@
                                             <div class="input-group-prepend">
                                                 <span class="input-group-text"><i class="fas fa-home"></i></span>
                                             </div>
-                                            <input class="form-control" placeholder="{{ __('CEP') }}" type="text" name="address_postalcode" required>
+                                            <input onblur="pesquisacep(this.value);" class="form-control" placeholder="{{ __('CEP') }}" type="text" name="address_postalcode" required>
                                         </div>
                                     </div>
                                 </div>
@@ -240,4 +240,73 @@
             </div>
         </div>
     </div>
+
+    <script>
+        function limpa_formulário_cep() {
+            //Limpa valores do formulário de cep.
+            $('input[name=address_street]').val("")
+            $('input[name=address_district]').val("")
+            $('input[name=address_city]').val("")
+            $('input[name=address_state]').val("")
+            $('input[name=address_country]').val("")
+        }
+
+        function meu_callback(conteudo) {
+            if (!("erro" in conteudo)) {
+                //Atualiza os campos com os valores.
+                $('input[name=address_street]').val(conteudo.logradouro)
+                $('input[name=address_district]').val(conteudo.bairro)
+                $('input[name=address_city]').val(conteudo.localidade)
+                $('input[name=address_state]').val(conteudo.uf)
+                $('input[name=address_country]').val("Brasil")
+            } //end if.
+            else {
+                //CEP não Encontrado.
+                limpa_formulário_cep();
+                alert("CEP não encontrado.");
+            }
+        }
+
+        function pesquisacep(valor) { 
+            //Nova variável "cep" somente com dígitos.
+            var cep = valor.replace(/\D/g, '');
+
+            //Verifica se campo cep possui valor informado.
+            if (cep != "") {
+
+                //Expressão regular para validar o CEP.
+                var validacep = /^[0-9]{8}$/;
+
+                //Valida o formato do CEP.
+                if(validacep.test(cep)) {
+
+                    //Preenche os campos com "..." enquanto consulta webservice.
+                    $('input[name=address_street]').val("...")
+                    $('input[name=address_district]').val("...")
+                    $('input[name=address_city]').val("...")
+                    $('input[name=address_state]').val("...")
+                    $('input[name=address_country]').val("...")
+
+                    //Cria um elemento javascript.
+                    var script = document.createElement('script');
+
+                    //Sincroniza com o callback.
+                    script.src = 'https://viacep.com.br/ws/'+ cep + '/json/?callback=meu_callback';
+
+                    //Insere script no documento e carrega o conteúdo.
+                    document.body.appendChild(script);
+
+                } //end if.
+                else {
+                    //cep é inválido.
+                    limpa_formulário_cep();
+                    alert("Formato de CEP inválido.");
+                }
+            } //end if.
+            else {
+                //cep sem valor, limpa formulário.
+                limpa_formulário_cep();
+            }
+        };
+    </script>
 @endsection
