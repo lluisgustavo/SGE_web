a5db67ac659951cd8af98377f40843f79b794f45
Luis Sousa <luis.sousa@giftconsult.com.br>
2021-07-30 13:55:41 -0300

    Address Model and Controller

---
+++ app/Address.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+
+class Address extends Model
+{
+
+    use Notifiable;
+
+    protected $table = "tb_address";
+    protected $primaryKey = "id";
+    public $timestamps = false;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'street', 'number', 'complement', 'district', 'city', 'state', 'postalcode', 'country', 'ref'
+    ]; 
+}
