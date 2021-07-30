6dd8346bec4fcc5ab1ad879b6b11b91f515c41d2
Luis Sousa <luis.sousa@giftconsult.com.br>
2021-07-30 11:20:39 -0300

    Removed global jQuery Mask plugin and will add it on each file that needs it

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,9 +36,8 @@
         @endguest
 
         <script src="{{ asset('argon') }}/vendor/jquery/dist/jquery.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js" integrity="sha512-pHVGpX7F/27yZ0ISY+VVjyULApbDlD0/X0rgGbTqCE7WFW5MezNTWG/dnhtbBuICzsd0WQPgpE4REBLv+UqChw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
         <script src="{{ asset('argon') }}/vendor/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
-        
+
         @stack('js')
         
         <!-- Argon JS -->
