d270b1f9225ff446c4fba22740d1011aa4199789
Luis Gustavo <lluisgusttavo@gmail.com>
2021-08-01 18:41:51 -0300

    Added 'Adicionar Usuário' only to admin

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -9,12 +9,14 @@
                     <div class="card-header border-0">
                         <div class="row align-items-center">
                             <div class="col-8">
-                                <h3 class="mb-0">Users</h3>
+                                <h3 class="mb-0">Usuários</h3>
                             </div>
 
-                            <div class="col-4 text-right">
-                                <a href="" class="btn btn-sm btn-primary">Add user</a>
-                            </div>
+                            @if(Auth::user()->role_id === 1)
+                                <div class="col-4 text-right">
+                                    <a href="{{ URL::to('users/create') }}" class="btn btn-sm btn-primary">Adicionar Usuário</a>
+                                </div>
+                            @endif
                         </div>
                     </div>
 
