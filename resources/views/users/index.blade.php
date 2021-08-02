7811bb4e8cb726eecc9569e115a41e4f5239b5df
Luis Gustavo <lluisgusttavo@gmail.com>
2021-08-01 21:27:52 -0300

    Format date and show ID

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    @include('layouts.headers.cards')
+    @include('layouts.headers.normal')
     <div class="container-fluid mt--7">
         <div class="row">
             <div class="col">
@@ -27,6 +27,7 @@
                         <table class="table align-items-center table-flush">
                             <thead class="thead-light">
                                 <tr>
+                                    <th scope="col">Id</th>
                                     <th scope="col">Nome</th>
                                     <th scope="col">Email</th>
                                     <th scope="col">Entrou em</th>
@@ -34,20 +35,21 @@
                                 </tr>
                             </thead>
                             <tbody>
-                                @foreach($users as $user):
+                                @foreach($users as $user)
                                 <tr>
+                                    <td>{{$user->user_id}} </td>
                                     <td>{{$user->first_name}} {{$user->last_name}} </td>
                                     <td>
                                         <a href="{{$user->email}}">{{$user->email}}</a>
                                     </td>
-                                    <td>{{$user->created_at}}</td>
+                                    <td>{{date("d/m/Y H:i:s", strtotime($user->created_at))}}</td>
                                     <td class="text-right">
                                         <div class="dropdown">
                                             <a class="btn btn-sm btn-icon-only text-light" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                                 <i class="fas fa-ellipsis-v"></i>
                                             </a>
                                             <div class="dropdown-menu dropdown-menu-right dropdown-menu-arrow">
-                                                <a class="dropdown-item" href="{{ URL::to('users/' . $user->id . '/edit') }}">Editar</a>
+                                                <a class="dropdown-item" href="{{ URL::to('users/' . $user->user_id . '/edit') }}">Editar</a>
                                             </div>
                                         </div>
                                     </td>
@@ -56,7 +58,7 @@
                             </tbody>
                         </table>
 
-                        {{ $users->links() }}
+                        <div class="d-flex justify-content-center">{{ $users->links() }}</div>
                     </div>
                     <div class="card-footer py-4">
                         <nav class="d-flex justify-content-end" aria-label="...">
