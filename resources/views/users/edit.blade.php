2d416b7537c3d4180021e9b75704f50824261266
Luis Gustavo <lluisgusttavo@gmail.com>
2021-08-01 22:32:29 -0300

    Edit page separated in 3 forms

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -19,125 +19,131 @@
                     </div>
 
                     <div class="card-body px-lg-5 py-lg-5">
-                            {{ $user }}
-                            {{ $userRole }}
-                            <form role="form" method="POST" action="{{ route('users.edit.user') }}">
-                            @csrf
+                        <div class="row">
+                            <div class="col-md-6">
+                                <!-- Dados Pessoais -->
+                                <div class="text-center mt-4">
+                                    <h2>{{ __('Conta') }}</h2>
+                                </div>
+                                <form role="form" method="POST" action="{{ route('users.edit.user') }}">
+                                @csrf
 
-                            <!-- Usuário -->
-                                <div class="form-group{{ $errors->has('email') ? ' has-danger' : '' }}">
-                                    <div class="input-group input-group-alternative mb-3">
-                                        <div class="input-group-prepend">
-                                            <span class="input-group-text"><i class="ni ni-email-83"></i></span>
+                                <!-- Usuário -->
+                                    <div class="form-group{{ $errors->has('email') ? ' has-danger' : '' }}">
+                                        <div class="input-group input-group-alternative mb-3">
+                                            <div class="input-group-prepend">
+                                                <span class="input-group-text"><i class="ni ni-email-83"></i></span>
+                                            </div>
+                                            <input class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" placeholder="{{ __('Email') }}" type="email" name="email" value="{{ $user->email }}" required>
                                         </div>
-                                        <input class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" placeholder="{{ __('Email') }}" type="email" name="email" value="{{ $user->email }}" required>
+                                        @if ($errors->has('email'))
+                                            <span class="invalid-feedback" style="display: block;" role="alert">
+                                            <strong>{{ $errors->first('email') }}</strong>
+                                        </span>
+                                        @endif
                                     </div>
-                                    @if ($errors->has('email'))
-                                        <span class="invalid-feedback" style="display: block;" role="alert">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                    @endif
-                                </div>
-                                <div class="form-group{{ $errors->has('password') ? ' has-danger' : '' }}">
-                                    <div class="input-group input-group-alternative">
-                                        <div class="input-group-prepend">
-                                            <span class="input-group-text"><i class="ni ni-lock-circle-open"></i></span>
+                                    <div class="form-group{{ $errors->has('password') ? ' has-danger' : '' }}">
+                                        <div class="input-group input-group-alternative">
+                                            <div class="input-group-prepend">
+                                                <span class="input-group-text"><i class="ni ni-lock-circle-open"></i></span>
+                                            </div>
+                                            <input class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" placeholder="{{ __('Senha') }}" type="password" name="password" required>
                                         </div>
-                                        <input class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" placeholder="{{ __('Senha') }}" type="password" name="password" required>
+                                        @if ($errors->has('password'))
+                                            <span class="invalid-feedback" style="display: block;" role="alert">
+                                            <strong>{{ $errors->first('password') }}</strong>
+                                        </span>
+                                        @endif
                                     </div>
-                                    @if ($errors->has('password'))
-                                        <span class="invalid-feedback" style="display: block;" role="alert">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                    @endif
-                                </div>
-                                <div class="form-group">
-                                    <div class="input-group input-group-alternative">
-                                        <div class="input-group-prepend">
-                                            <span class="input-group-text"><i class="ni ni-lock-circle-open"></i></span>
+                                    <div class="form-group">
+                                        <div class="input-group input-group-alternative">
+                                            <div class="input-group-prepend">
+                                                <span class="input-group-text"><i class="ni ni-lock-circle-open"></i></span>
+                                            </div>
+                                            <input class="form-control" placeholder="{{ __('Confirmar Senha') }}" type="password" name="password_confirmation" required>
                                         </div>
-                                        <input class="form-control" placeholder="{{ __('Confirmar Senha') }}" type="password" name="password_confirmation" required>
                                     </div>
-                                </div>
 
-                                <div class="text-center">
-                                    <button type="submit" class="btn btn-primary mt-4">{{ __('Editar Conta') }}</button>
-                                </div>
-                            </form>
+                                    <div class="text-center">
+                                        <button type="submit" class="btn btn-primary mt-4">{{ __('Editar Conta') }}</button>
+                                    </div>
+                                </form>
+                            </div>
 
+                            <div class="col-md-6">
                                 <!-- Dados Pessoais -->
                                 <div class="text-center mt-4">
                                     <h2>{{ __('Dados Pessoais') }}</h2>
                                 </div>
 
-                            <form role="form" method="POST" action="{{ route('users.edit.person') }}">
-                                @csrf
-                                <div class="row">
-                                    <div class="col-md-6">
-                                        <div class="form-group">
-                                            <div class="input-group input-group-alternative">
-                                                <div class="input-group-prepend">
-                                                    <span class="input-group-text"><i class="fas fa-user"></i></span>
+                                <form role="form" method="POST" action="{{ route('users.edit.person') }}">
+                                    @csrf
+                                    <div class="row">
+                                        <div class="col-md-6">
+                                            <div class="form-group">
+                                                <div class="input-group input-group-alternative">
+                                                    <div class="input-group-prepend">
+                                                        <span class="input-group-text"><i class="fas fa-user"></i></span>
+                                                    </div>
+                                                    <input class="form-control" placeholder="{{ __('Nome') }}" type="text" value="{{ $user->first_name }}" name="person_first_name" required>
                                                 </div>
-                                                <input class="form-control" placeholder="{{ __('Nome') }}" type="text" value="{{ $user->first_name }}" name="person_first_name" required>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <div class="form-group">
+                                                <div class="input-group input-group-alternative">
+                                                    <div class="input-group-prepend">
+                                                        <span class="input-group-text"><i class="fas fa-user"></i></span>
+                                                    </div>
+                                                    <input class="form-control" placeholder="{{ __('Sobrenome') }}" type="text" value="{{ $user->last_name }}" name="person_last_name" required>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
-                                    <div class="col-md-6">
-                                        <div class="form-group">
-                                            <div class="input-group input-group-alternative">
-                                                <div class="input-group-prepend">
-                                                    <span class="input-group-text"><i class="fas fa-user"></i></span>
-                                                </div>
-                                                <input class="form-control" placeholder="{{ __('Sobrenome') }}" type="text" value="{{ $user->last_name }}" name="person_last_name" required>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
 
-                                <div class="row">
-                                    <div class="col-md-4">
-                                        <div class="form-group">
-                                            <div class="input-group input-group-alternative">
-                                                <div class="input-group-prepend">
-                                                    <span class="input-group-text"><i class="fas fa-user"></i></span>
+                                    <div class="row">
+                                        <div class="col-md-6">
+                                            <div class="form-group">
+                                                <div class="input-group input-group-alternative">
+                                                    <div class="input-group-prepend">
+                                                        <span class="input-group-text"><i class="fas fa-user"></i></span>
+                                                    </div>
+                                                    <input onblur="ValidaCPF(this)" class="form-control ob" placeholder="{{ __('CPF') }}" value="{{ $user->cpf }}" type="text" name="person_cpf" required>
                                                 </div>
-                                                <input onblur="ValidaCPF(this)" class="form-control ob" placeholder="{{ __('CPF') }}" value="{{ $user->cpf }}" type="text" name="person_cpf" required>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <div class="form-group">
+                                                <div class="input-group input-group-alternative">
+                                                    <div class="input-group-prepend">
+                                                        <span class="input-group-text"><i class="fas fa-calendar-alt"></i></span>
+                                                    </div>
+                                                    <input class="form-control ob" placeholder="{{ __('Data de Nascimento') }}" type="date" value="{{ $user->birthday }}" data-mask="0000-0000" name="person_birthday" required>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
-                                    <div class="col-md-4">
-                                        <div class="form-group">
-                                            <div class="input-group input-group-alternative">
-                                                <div class="input-group-prepend">
-                                                    <span class="input-group-text"><i class="fas fa-calendar-alt"></i></span>
-                                                </div>
-                                                <input class="form-control ob" placeholder="{{ __('Data de Nascimento') }}" type="date" value="{{ $user->birthday }}" data-mask="0000-0000" name="person_birthday" required>
+                                    <div class="form-group">
+                                        <div class="input-group input-group-alternative">
+                                            <div class="input-group-prepend">
+                                                <span class="input-group-text"><i class="fas fa-phone"></i></span>
                                             </div>
+                                            <input class="form-control ob" placeholder="{{ __('Telefone') }}" type="tel" value="{{ $user->phone }}" name="person_phone" required>
                                         </div>
                                     </div>
-                                    <div class="col-md-4">
-                                        <div class="form-group">
-                                            <div class="input-group input-group-alternative">
-                                                <div class="input-group-prepend">
-                                                    <span class="input-group-text"><i class="fas fa-phone"></i></span>
-                                                </div>
-                                                <input class="form-control ob" placeholder="{{ __('Telefone') }}" type="tel" value="{{ $user->phone }}" name="person_phone" required>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
 
-                                <div class="text-center">
-                                    <button type="submit" class="btn btn-primary mt-4">{{ __('Editar Dados Pessoais') }}</button>
-                                </div>
-                            </form>
-                                <!-- Dados Pessoais -->
+                                    <div class="text-center">
+                                        <button type="submit" class="btn btn-primary mt-4">{{ __('Editar Dados Pessoais') }}</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                        <!-- Dados Pessoais -->
 
-                                <!-- Endereço -->
-                                <div class="text-center mt-4">
-                                    <h2>{{ __('Endereço') }}</h2>
-                                </div>
+                            <!-- Endereço -->
+                            <div class="text-center mt-4">
+                                <h2>{{ __('Endereço') }}</h2>
+                            </div>
 
                             <form role="form" method="POST" action="{{ route('users.edit.address') }}">
                                 @csrf
