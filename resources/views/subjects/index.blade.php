63aac85058318f2f56554cfc2a8fd975f9cad08e
Luis Sousa <luis.sousa@giftconsult.com.br>
2021-08-10 16:51:56 -0300

    Adjusting subjects variables of index

--- resources/views/subjects/index.blade.php
+++ resources/views/subjects/index.blade.php
@@ -9,7 +9,7 @@
                     <div class="card-header border-0">
                         <div class="row align-items-center">
                             <div class="col-8">
-                                <h3 class="mb-0">Cursos</h3>
+                                <h3 class="mb-0">Disciplinas</h3>
                             </div>
 
                             @if(Auth::user()->role_id === 1)
