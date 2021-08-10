1d7a1ea3e343af1c7385d1fcc0b82a3006f0d652
Luis Sousa <luis.sousa@giftconsult.com.br>
2021-08-10 16:34:51 -0300

    Model configured

--- app/Subject.php
+++ app/Subject.php
@@ -8,5 +8,28 @@ use Spatie\Permission\Traits\HasRoles;
 
 class Subject extends Model
 {
-    //
+    use Notifiable;
+    use HasRoles;
+
+    protected $table = "tb_subjects";
+    protected $primaryKey = "id";
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'id', 'name', 'hourly_load', 'syllabus', 'teacher_id'
+    ];
+
+    public function course()
+    {
+        return $this->hasOne(Course::class);
+    }
+
+    public function person()
+    {
+        return $this->hasOne(Person::class);
+    }
 }
