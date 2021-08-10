553f9c9874fea57e601bbd9045c6b363807c3fc7
Luis Sousa <luis.sousa@giftconsult.com.br>
2021-08-10 16:35:08 -0300

    Initial table of subjects

--- resources/views/subjects/index.blade.php
+++ resources/views/subjects/index.blade.php
@@ -14,7 +14,7 @@
 
                             @if(Auth::user()->role_id === 1)
                                 <div class="col-4 text-right">
-                                    <a href="{{ route('courses.create')  }}" class="btn btn-sm btn-primary">Adicionar Curso</a>
+                                    <a href="{{ route('subjects.create')  }}" class="btn btn-sm btn-primary">Adicionar Disciplina</a>
                                 </div>
                             @endif
                         </div>
@@ -30,23 +30,22 @@
                                     <th scope="col">Id</th>
                                     <th scope="col">Nome</th>
                                     <th scope="col">Carga Horária</th>
-                                    <th scope="col">Departamento</th>
+                                    <th scope="col">Cursos</th>
                                     <th scope="col"></th>
                                 </tr>
                             </thead>
                             <tbody>
-                                @foreach($courses as $course)
+                                @foreach($subjects as $subject)
                                 <tr>
-                                    <td>{{$course->id}} </td>
-                                    <td>{{$course->name}} </td>
-                                    <td>{{($course->hourly_load ? $course->hourly_load . 'h' : '00h')}} </td>
-                                    <td>{{$course->department_name}} </td>
+                                    <td>{{$subject->id}} </td>
+                                    <td>{{$subject->name}} </td>
+                                    <td>{{($subject->hourly_load ? $subject->hourly_load . 'h' : '00h')}} </td>
                                     <td class="text-right">
                                         <div class="row">
-                                            <a class="" href="{{ route('courses.edit', $course->id) }}"><i class="fas fa-pencil-alt"></i></a>
+                                            <a class="" href="{{ route('subjects.edit', $subject->id) }}"><i class="fas fa-pencil-alt"></i></a>
                                             <a href="javascript:void(0);" onclick="$(this).find('form').submit();" style="margin-left: 0.2em;">
                                                 <i class="fas fa-trash"></i>
-                                                <form style="display: none" action="{{ route('courses.destroy', $course->id) }}" method="POST">
+                                                <form style="display: none" action="{{ route('subjects.destroy', $subject->id) }}" method="POST">
                                                     @method('DELETE')
                                                     @csrf
                                                 </form>
@@ -58,7 +57,7 @@
                             </tbody>
                         </table>
 
-                        <div class="d-flex justify-content-center">{{ $courses->links() }}</div>
+                        <div class="d-flex justify-content-center">{{ $subjects->links() }}</div>
                     </div>
                     <div class="card-footer py-4">
                         <nav class="d-flex justify-content-end" aria-label="...">
