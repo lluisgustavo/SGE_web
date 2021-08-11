65ded298287c0292679121d0e26245dae206419a
Luis Sousa <luis.sousa@giftconsult.com.br>
2021-08-11 14:39:09 -0300

    View page with selected values from database

--- resources/views/subjects/edit.blade.php
+++ resources/views/subjects/edit.blade.php
@@ -9,7 +9,7 @@
                     <div class="card-header border-0">
                         <div class="row align-items-center">
                             <div class="col">
-                                <h3 class="mb-0">Editar Curso</h3>
+                                <h3 class="mb-0">Editar Disciplina</h3>
                             </div>
                         </div>
                     </div>
@@ -18,44 +18,68 @@
                     </div>
 
                     <div class="card-body px-lg-5 py-lg-5">
-                        <!-- Curso -->
-                        <form role="form" method="POST" action="{{ route('courses.update', $course->id) }}">
+                        <!-- Disciplina -->
+                        <div class="col-md-12">
+                            <form role="form" method="POST" action="{{ route('subjects.update', $subject->id) }}">
                             @csrf
 
-                            <div class="form-group{{ $errors->has('course') ? ' has-danger' : '' }}">
-                                <div class="input-group input-group-alternative mb-3">
-                                    <div class="input-group-prepend">
-                                        <span class="input-group-text"><i class="fas fa-sticky-note"></i></span>
-                                    </div>
-                                    <input class="form-control{{ $errors->has('course') ? ' is-invalid' : '' }}" placeholder="{{ __('Nome do Curso') }}" type="text" name="name" value="{{$course->name}}" required>
+                                <div class="text-center">
+                                    <h2>{{ __('Disciplina') }}</h2>
                                 </div>
-                                @if ($errors->has('course'))
-                                    <span class="invalid-feedback" style="display: block;" role="alert">
-                                            <strong>{{ $errors->first('course') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-
-                            <div class="form-group">
-                                <div class="input-group input-group-alternative">
-                                    <div class="input-group-prepend">
-                                        <span class="input-group-text"><i class="fas fa-user"></i></span>
+                                <div class="row">
+                                    <div class="col-md-10 form-group{{ $errors->has('subject') ? ' has-danger' : '' }}">
+                                        <div class="input-group input-group-alternative mb-3">
+                                            <div class="input-group-prepend">
+                                                <span class="input-group-text"><i class="fas fa-sticky-note"></i></span>
+                                            </div>
+                                            <input class="form-control{{ $errors->has('subject') ? ' is-invalid' : '' }}" placeholder="{{ __('Nome da Disciplina') }}" type="text" name="name" value="{{ $subject->name }}" required>
+                                        </div>
+                                        @if ($errors->has('subject'))
+                                            <span class="invalid-feedback" style="display: block;" role="alert">
+                                                <strong>{{ $errors->first('subject') }}</strong>
+                                            </span>
+                                        @endif
                                     </div>
-                                    <select class="browser-default custom-select" name="department_id" required>
-                                        <option>Selecione um Departamento</option>
-                                        @foreach($departments as $department)
-                                            <option value="{{$department->id}}" {{($course->department_id == $department->id) ? "selected" : ''}}>
-                                                {{ $department->name }}
-                                            </option>
-                                        @endforeach
-                                    </select>
+                                    <div class="col-md-2 form-group{{ $errors->has('subject') ? ' has-danger' : '' }}">
+                                        <div class="input-group input-group-alternative mb-3">
+                                            <div class="input-group-prepend">
+                                                <span class="input-group-text"><i class="fas fa-clock"></i></span>
+                                            </div>
+                                            <input class="form-control{{ $errors->has('subject') ? ' is-invalid' : '' }}" placeholder="{{ __('Carga horária') }}" type="number" name="hourly_load" value="{{ $subject->hourly_load }}" required>
+                                        </div>
+                                        @if ($errors->has('subject'))
+                                            <span class="invalid-feedback" style="display: block;" role="alert">
+                                                <strong>{{ $errors->first('subject') }}</strong>
+                                            </span>
+                                        @endif
+                                    </div>
                                 </div>
-                            </div>
+                                <div class="form-group">
+                                    <div class="input-group input-group-alternative">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text"><i class="fas fa-user"></i></span>
+                                        </div>
+                                        <select multiple="multiple" class="browser-default custom-select" name="courses[]" required>
+                                            <option>Selecione os Cursos</option>
+                                            @foreach($courses as $course)
+                                                <option value="{{$course->id}}"
+                                                        @foreach($subject_courses as $subject_course)
+                                                        @if($subject_course->course_id === $course->id)
+                                                        selected
+                                                        @endif
+                                                        @endforeach>
+                                                    {{ $course->name }}
+                                                </option>
+                                            @endforeach
+                                        </select>
+                                    </div>
+                                </div>
 
-                            <div class="text-center">
-                                <button type="submit" class="btn btn-primary">{{ __('Editar Curso') }}</button>
-                            </div>
-                        </form>
+                                <div class="text-center">
+                                    <button type="submit" class="btn btn-primary">{{ __('Editar Disciplina') }}</button>
+                                </div>
+                            </form>
+                        </div>
                     </div>
                     <div class="card-footer py-4">
                         <nav class="d-flex justify-content-end" aria-label="...">
